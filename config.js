3c4cd4c79c851c7540169136ebf083c70fffe5b0
npool.pw <36919430+npoolpw@users.noreply.github.com>
2018-05-10 15:42:59 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+var api = "http://sg2.npool.pw:2260";
+
+var poolHost = "sg2.npool.pw";
+
+var irc = "irc.freenode.net/#spescoin";
+
+var email = "support@myspes.org";
+
+var cryptonatorWidget = ["BTC-USD", "SPES-BTC"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "http://pool.myspes.org/?hash={id}#blockchain_block";
+
+var transactionExplorer = "http://pool.myspes.org/?hash={id}#blockchain_transaction";
+
+var themeCss = "themes/default-theme.css";
